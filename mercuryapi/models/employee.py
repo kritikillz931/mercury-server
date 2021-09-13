de945a4527a798b6a147a0639ec537c4e5a1df72
Justin <justinadams758@gmail.com>
2021-09-13 15:24:28 -0500

    employee viewset complete

--- mercuryapi/models/employee.py
+++ mercuryapi/models/employee.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User #pylint:disable=(imported-auth-user)
 class Employee(models.Model):
     """Employee Model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    position = models.CharField(max_length=100, null=True)
+    position = models.CharField(max_length=100 )
     dateHired = models.DateField()
-    monthlySales = models.IntegerField(null=True)
-    department = models.ForeignKey("department", on_delete=models.CASCADE, null=True)
+    monthlySales = models.IntegerField()
+    department = models.ForeignKey("department", on_delete=models.CASCADE)
--- mercuryapi/views/__init__.py
+++ mercuryapi/views/__init__.py
@@ -1,4 +1,4 @@
 from .auth import login_user
 from .auth import register_user
-from .employee import Employee
+from .employee import EmployeeView
 from .monthlyfinance import MonthlyFinanceView
